efce2dfebea3cb82c1a1f846e720a409ab82d352
KlopovAleksey <Klopov.AN@phystech.edu>
2019-11-21 16:18:08 +0300

    .

--- solar_objects.py
+++ solar_objects.py
@@ -4,16 +4,16 @@
 
 class Star:
     def __init__(self, type, m, x, y, Vx, Vy, Fx, Fy, R, color, image):
-    self.x = x
-    self.type = type
-    self.y = y
-    self.Vx = x
-    self.Vy = y
-    self.Fx = x
-    self.Fy = y
-    self.R = R
-    self.color = color
-    self.image = image
+        self.x = x
+        self.type = type
+        self.y = y
+        self.Vx = x
+        self.Vy = y
+        self.Fx = x
+        self.Fy = y
+        self.R = R
+        self.color = color
+        self.image = image
     """Тип данных, описывающий звезду.
     Содержит массу, координаты, скорость звезды,
     а также визуальный радиус звезды в пикселах и её цвет.
@@ -22,16 +22,17 @@ class Star:
 
 class Planet:
     def __init__(self, type, m, x, y, Vx, Vy, Fx, Fy, R, color, image):
-    self.x = x
-    self.type = type
-    self.y = y
-    self.Vx = x
-    self.Vy = y
-    self.Fx = x
-    self.Fy = y
-    self.R = R
-    self.color = color
-    self.image = image
+        self.x = x
+        self.type = type
+        self.y = y
+        self.Vx = x
+        self.Vy = y
+        self.Fx = x
+        self.Fy = y
+        self.R = R
+        self.color = color
+        self.image = image
+
     """Тип данных, описывающий планету.
     Содержит массу, координаты, скорость планеты,
     а также визуальный радиус планеты в пикселах и её цвет
